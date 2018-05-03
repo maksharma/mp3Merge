470f5780dfdcbef586c106832cc5639e11e8eeef
Mayank Sharma <maksharma231@gmail.com>
2018-05-03 15:05:35 +0800

    Update mergeMp3.js
    cosmetic updates.

--- mergeMp3.js
+++ mergeMp3.js
@@ -7,17 +7,17 @@ const ffmpeg = require('@ffmpeg-installer/ffmpeg');
 var mp3Files = [
 	'SampleAudio_0.4mb.mp3',
 	'SampleAudio_0.7mb.mp3',
-]
+];
 
 audioconcat(mp3Files)
 	.concat('final_output.mp3')
 	.on('start', function (command) {
-		console.log('ffmpeg process started:', command)
+		console.log('ffmpeg process started:', command);
 	})
 	.on('error', function (err, stdout, stderr) {
 		console.error('Error:', err)
-		console.error('ffmpeg stderr:', stderr)
+		console.error('ffmpeg stderr:', stderr);
 	})
 	.on('end', function (output) {
-		console.error('Audio created in:', output)
+		console.error('Audio created in:', output);
 	});
