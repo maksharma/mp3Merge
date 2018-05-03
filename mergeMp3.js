bb48c4d8dff220148a5b93d212d135f5b22913e7
Mayank Sharma <maksharma231@gmail.com>
2018-05-03 14:56:52 +0800

    merge multiple mp3 files

---
+++ mergeMp3.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var audioconcat = require('audioconcat');
+const ffmpeg = require('@ffmpeg-installer/ffmpeg');
+
+
+var mp3Files = [
+	'/Users/mayanksharma/Downloads/SampleAudio_0.4mb.mp3',
+	'/Users/mayanksharma/Downloads/SampleAudio_0.7mb.mp3',
+]
+
+audioconcat(mp3Files)
+	.concat('final_output.mp3')
+	.on('start', function (command) {
+		console.log('ffmpeg process started:', command)
+	})
+	.on('error', function (err, stdout, stderr) {
+		console.error('Error:', err)
+		console.error('ffmpeg stderr:', stderr)
+	})
+	.on('end', function (output) {
+		console.error('Audio created in:', output)
+	});
